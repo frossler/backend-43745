874af51f0ef66df9d8bbf18291490aae689d745d
Fernando Rossler <frossler89@gmail.com>
2023-11-15 16:56:55 -0300

    updates

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -3,14 +3,16 @@ socket.on('products', (data) => {
   const productsList = document.querySelector(".container");
   productsList.innerHTML = " ";
   data.forEach((element) => {
+    const thumbnails = element.thumbnails.map(thumbnail => `<img src=${thumbnail} style="width: 50px;"></img>`).join('');
     const boxItem = `
-                <div class="product">
-                <h3>${element.title}</h3>
-                <p>Description: ${element.description}</p>
-                <p>ID: ${element.id}</p>
-                <p>$ ${element.price}</p>
-                <p>Stock: ${element.stock}</p>
-                </div>`;
+      <div class="product">
+        <h3>${element.title}</h3>
+        ${thumbnails}
+        <p>Code: ${element.code}</p>
+        <p>Description: ${element.description}</p>
+        <p>ID: ${element.id}</p>
+        <p>$ ${element.price}</p>
+      </div>`;
     productsList.innerHTML += boxItem;
   });
 });
